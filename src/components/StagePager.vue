c01816ec03a5abbf1fa6933e0041a5c6320a380b
Yahya Aqrom <aqrom.yahya75@gmail.com>
2026-02-25 01:10:23 +0700

    Remove Swiper Pagination Indicator

--- src/components/StagePager.vue
+++ src/components/StagePager.vue
@@ -1,19 +1,15 @@
 <script setup lang="ts">
 import { Swiper, SwiperSlide } from 'swiper/vue';
-import { Pagination } from 'swiper/modules';
 import { stages } from '../data/stages';
 import StagePage from './StagePage.vue';
 
 import 'swiper/css';
-import 'swiper/css/pagination';
 </script>
 
 <template>
   <Swiper
-    :modules="[Pagination]"
     :slides-per-view="1"
     :space-between="0"
-    :pagination="{ clickable: true }"
     class="h-full bg-slate-100"
   >
     <SwiperSlide v-for="(stage, index) in stages" :key="stage.id" class="h-full bg-slate-100">
